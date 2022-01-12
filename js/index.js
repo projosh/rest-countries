f3be9372451c6bcace930622a75824fe90f7164e
Steve BERENGER <steve.berenger@gmail.com>
2022-01-12 14:24:46 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+async function fetchCancan (){
+    const response = await fetch('https://restcountries.com/v3.1/region/europe/?fields=name');
+
+
+    const countries = await response.json();
+    let name = "";
+
+    countries.forEach(country => {
+
+        name += `<li> ${country.name.official}</li>`;
+        
+        
+    });
+
+    const querySelector = document.querySelector("#countries");
+    querySelector.innerHTML = name;
+    
+}
+window.addEventListener("load" , fetchCancan());
+
