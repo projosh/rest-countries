a22a808ae1921e76f5ddf30b0d7065e8b003d97f
Steve BERENGER <steve.berenger@gmail.com>
2022-01-18 16:51:59 +0100

    Les parties 1 et 2

--- scripts/table.js
+++ scripts/table.js
@@ -1,7 +1,39 @@
+async function tableau(){
+    const reponse = await fetch ('https://restcountries.com/v3.1/region/europe/?fields=name,area,population,capital');
+    // console.log(reponse);
+
+    const pays = await reponse.json();
+    console.log(pays);
+    let kindercountry = "";
+    // console.log(country);
+
+    const number = new Intl.NumberFormat("en");
+    
+    
+    for (const country of pays) {
+        kindercountry +=  `<tr><th class="text-nowrap"> ${country.name.official}</th><td> ${number.format(country.area)}</td>
+                            <td>${number.format(country.population)}</td> <td class="text-nowrap">${country.capital}</td></tr>`;
+                                
+    };
 
 
-const myObj = {firstname:"John", lastname:"Doe"};
-console.log(myObj);
+
+    const querySelector = document.querySelector("#array");
+    querySelector.innerHTML = kindercountry;
+    // console.log(country);
+    
 
 
+}
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load' , tableau);
 
